5e627cb46eb14805468f35986d3a7bc4c19815c1
Lorena <lorena.giddens@outlook.com>
2018-06-28 17:04:50 +0000

    Stub out the CLI class with hard-coded return data

--- lib/best_books/cli.rb
+++ lib/best_books/cli.rb
@@ -2,6 +2,15 @@ class BestBooks::CLI
 
   def call
     puts "Best Novels:"
+    list_books
+  end
+
+  def list_books
+    puts <<-DOC.gsub /^\s*/, ''
+    # change this
+    1. One Hundred Years of Solitude by Gabriel Garcia Marquez - $10.04
+    2. Tender is the Night by F.Scott Fitzgerald - $16.54
+    DOC
   end
 
 end
