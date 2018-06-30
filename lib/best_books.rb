c1096755f4fd29f09a77d9c04f182123a01f1f13
Lorena <lorena.giddens@outlook.com>
2018-06-30 16:34:57 +0000

    Set up scrape methods for both websites and add dependencies

--- lib/best_books.rb
+++ lib/best_books.rb
@@ -1,6 +1,7 @@
-# require 'open-uri'
-# require 'nokogiri'
-# require 'pry'
+require 'open-uri'
+require 'rubygems'
+require 'nokogiri'
+require 'pry'
 
 require_relative "best_books/version"
 require_relative "./best_books/cli"
--- lib/best_books/books.rb
+++ lib/best_books/books.rb
@@ -12,35 +12,51 @@ class BestBooks::Books
   end
 
   def self.scrape_editors
-    book_1 = self.new
-    book_1.title = "One Hundred Years of Solitude"
-    book_1.author = "Gabriel Garcia Marquez"
-    book_1.price = "$10.04"
-    book_1.url = "https://www.bookdepository.com/One-Hundred-Years-Solitude-Gabriel-Garcia-Marquez/9780141184999"
+    editors_books = []
 
-    book_2 = self.new
-    book_2.title = "Tender is the Night"
-    book_2.author = "F.Scott Fitzgerald"
-    book_2.price = "$16.54"
-    book_2.url = "https://www.bookdepository.com/Tender-is-Night-F-Scott-Fitzgerald/9780141194066"
+    # go to https://www.bookdepository.com/bestbooksever
+    # extract the properties
+    # instantiate a book
 
-    [book_1, book_2]
+    editors_books
+
+    # book_1 = self.new
+    # book_1.title = "One Hundred Years of Solitude"
+    # book_1.author = "Gabriel Garcia Marquez"
+    # book_1.price = "$10.04"
+    # book_1.url = "https://www.bookdepository.com/One-Hundred-Years-Solitude-Gabriel-Garcia-Marquez/9780141184999"
+    #
+    # book_2 = self.new
+    # book_2.title = "Tender is the Night"
+    # book_2.author = "F.Scott Fitzgerald"
+    # book_2.price = "$16.54"
+    # book_2.url = "https://www.bookdepository.com/Tender-is-Night-F-Scott-Fitzgerald/9780141194066"
+    #
+    # [book_1, book_2]
   end
 
   def self.scrape_readers
-    book_1 = self.new
-    book_1.title = "Parade's End"
-    book_1.author = "Ford Madox Ford"
-    book_1.price = "$13.68"
-    book_1.url = "https://www.bookdepository.com/Parades-End-Ford-Madox-Ford/9781849904933"
+    readers_books = []
 
-    book_2 = self.new
-    book_2.title = "The Princess Bride"
-    book_2.author = "William Goldman"
-    book_2.price = "$7.82"
-    book_2.url = "https://www.bookdepository.com/Princess-Bride-William-Goldman/9780747590583"
+    # go to https://www.bookdepository.com/yourbestbooksever
+    # extract the properties
+    # instantiate a book
 
-    [book_1, book_2]
+    readers_books
+
+    # book_1 = self.new
+    # book_1.title = "Parade's End"
+    # book_1.author = "Ford Madox Ford"
+    # book_1.price = "$13.68"
+    # book_1.url = "https://www.bookdepository.com/Parades-End-Ford-Madox-Ford/9781849904933"
+    #
+    # book_2 = self.new
+    # book_2.title = "The Princess Bride"
+    # book_2.author = "William Goldman"
+    # book_2.price = "$7.82"
+    # book_2.url = "https://www.bookdepository.com/Princess-Bride-William-Goldman/9780747590583"
+    #
+    # [book_1, book_2]
   end
 
 
